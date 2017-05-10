8da255a7517391811362ab78f93a38bf97b10fb4
Colin McKeon <colin.mckeon@uga.edu>
2017-05-10 15:08:55 -0400

    Fixed issue with Ruby and Compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,18 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');	
 
 
-var coffeeSources = ['components/coffee/tagline.coffee']
+var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'	
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -26,3 +28,16 @@ gulp.task('js', function() {
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'))
+});
+
+
